2d8754b21b9f4d932890e04db96877801eca698b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-13 16:11:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,6 @@
-require 'pry'
+
 def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender = F;"
-  binding.pry
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
